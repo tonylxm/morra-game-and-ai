aed1b6834a6f72e47aa8ebe16f110c3b0a6b953d
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-05-14 20:01:15 +1200

    Task 6 complete. All test cases given passing. showStats is functional. Next steps: check corner cases and make own test cases to fully check game is functioning as intended.

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -11,7 +11,11 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Morra {
 
   private String playerName;
-  private int roundNum = 0;
+  private String aiName = "Jarvis";
+  private int roundNum = 0; // when roundNum = 0, this indicates no game is currently active
+  private int winPoints;
+  private int humanWins;
+  private int aiWins;
   private Difficulty level;
   private List<Integer> fingerHistory;
 
@@ -20,8 +24,12 @@ public class Morra {
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
     playerName = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(playerName);
-    // Keep track of number of rounds in this game
+    // Keep track of number of rounds in this game and points to win
     roundNum = 1;
+    winPoints = pointsToWin;
+    // Reset score tally both to 0
+    humanWins = 0;
+    aiWins = 0;
     level = difficulty;
     fingerHistory = new ArrayList<Integer>();
   }
@@ -45,17 +53,28 @@ public class Morra {
       int[] choicesHuman = player.play();
       int[] choicesAi = ai.play(roundNum, fingerHistory);
 
-      // Calculate and display outcome
+      // Calculate and display results of round
       fingerHistory.add(choicesHuman[0]);
-      int result = calculateResult(choicesHuman, choicesAi);
+      calculateResult(choicesHuman, choicesAi);
 
+      // After the current round of play is over, check if there is a winner
+      isThereAWinner();
       roundNum += 1;
     }
   }
 
-  public void showStats() {}
+  public void showStats() {
+    if (roundNum == 0) {
+      MessageCli.GAME_NOT_STARTED.printMessage();
+    } else {
+      MessageCli.PRINT_PLAYER_WINS.printMessage(
+          playerName, String.valueOf(humanWins), String.valueOf(winPoints - humanWins));
+      MessageCli.PRINT_PLAYER_WINS.printMessage(
+          aiName, String.valueOf(aiWins), String.valueOf(winPoints - aiWins));
+    }
+  }
 
-  private int calculateResult(int[] human, int[] cpu) {
+  private void calculateResult(int[] human, int[] cpu) {
     // HUMAN_WINS = 0
     // CPU_WINS = 1
     // DRAW = 2
@@ -65,16 +84,28 @@ public class Morra {
     // If both guess correctly or no one guesses correctly, then it is a DRAW
     if (totalSum == human[1] && totalSum == cpu[1]) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("DRAW");
-      return 2;
     } else if (totalSum == human[1]) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("HUMAN_WINS");
-      return 0;
+      humanWins += 1;
     } else if (totalSum == cpu[1]) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("AI_WINS");
-      return 1;
+      aiWins += 1;
     } else {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("DRAW");
-      return 2;
+    }
+  }
+
+  private void isThereAWinner() {
+    if (humanWins == winPoints) {
+      MessageCli.END_GAME.printMessage(playerName, String.valueOf(roundNum));
+      roundNum =
+          -1; // Game over, so reset roundNum to end current game (roundNum = 0 at the end of play
+      // function)
+    } else if (aiWins == winPoints) {
+      MessageCli.END_GAME.printMessage(aiName, String.valueOf(roundNum));
+      roundNum =
+          -1; // Game over, so reset roundNum to end current game (roundNum = 0 at the end of play
+      // function)
     }
   }
 }
